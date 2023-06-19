03f4a3b02843eb28773ae0d1c9f36a59568f0537
Scott Schwab <scottschwab86@gmail.com>
2023-06-19 11:45:00 -0500

    Complete core deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [allSushi, setAllSushi] = useState([]);
+  const [sushiEaten, setSushiEaten] = useState([]);
+  const [moneyRemaining, setMoneyRemaining] = useState(100);
+
+  console.log('SUSHI EATEN', sushiEaten)
+
+  function eatSushi(consumedSushi) {
+    setMoneyRemaining((prevMoney) => prevMoney - consumedSushi.price);
+    setSushiEaten((prevEaten) => {
+      setSushiEaten([...sushiEaten, consumedSushi]);
+    })
+
+  }
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(sushi => setAllSushi(sushi));
+  }, [])
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer allSushi={allSushi} eatSushi={eatSushi} moneyRemaining={moneyRemaining} />
+      <Table moneyRemaining={moneyRemaining} plates={sushiEaten} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi';
 
-function SushiContainer(props) {
+function SushiContainer({ allSushi, eatSushi, moneyRemaining }) {
+
+  const [startingID, setStartingID] = useState(1);
+
+  const filteredSushi = allSushi.filter((sushi) => sushi.id >= startingID && sushi.id < (startingID + 4));
+  const nextFourSushi = filteredSushi.map((sushi) =>
+    // <Sushi
+    //   key={sushi.id}
+    //   name={sushi.name}
+    //   image={sushi.img_url}
+    //   price={sushi.price}
+    //   eatSushi={eatSushi}
+    //   moneyRemaining={moneyRemaining}
+    // />);
+    <Sushi
+      key={sushi.id}
+      sushi={sushi}
+      eatSushi={eatSushi}
+      moneyRemaining={moneyRemaining}
+    />);
+
+  function getNextFourSushi() {
+    setStartingID((prevID) => prevID + 4);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {nextFourSushi}
+      <MoreButton getNextFourSushi={getNextFourSushi} />
     </div>
   );
 }
